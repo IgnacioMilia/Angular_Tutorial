555625d00f709f0fcb288882c0c8a52306ea02ef
imili <ignaciomilia91@gmail.com>
2024-01-15 21:52:52 -0300

    Agregado selecteable para agregar a dashboard

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice', selected: true},
-  { id: 13, name: 'Bombasto', selected: true},
-  { id: 14, name: 'Celeritas', selected: false},
-  { id: 15, name: 'Magneta', selected: false},
-  { id: 16, name: 'RubberMan', selected: false},
-  { id: 17, name: 'Dynama', selected: false},
-  { id: 18, name: 'Dr. IQ', selected: false},
-  { id: 19, name: 'Magma', selected: false},
-  { id: 20, name: 'Tornado', selected: false}
+  { id: 12, name: 'Dr. Nice', selected: true, power: 1},
+  { id: 13, name: 'Bombasto', selected: true, power: 1},
+  { id: 14, name: 'Celeritas', selected: false, power: 1},
+  { id: 15, name: 'Magneta', selected: false, power: 1},
+  { id: 16, name: 'RubberMan', selected: false, power: 1},
+  { id: 17, name: 'Dynama', selected: false, power: 1},
+  { id: 18, name: 'Dr. IQ', selected: false, power: 1},
+  { id: 19, name: 'Magma', selected: false, power: 1},
+  { id: 20, name: 'Tornado', selected: false, power: 1}
 ];
